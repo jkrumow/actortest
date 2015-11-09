1c6f8ee96f2bb047432a5c645e37c46daf4a2ec0
Julian Krumow <julian.krumow@tarbrain.com>
2015-11-09 19:26:31 +0100

    Initial Commit

---
+++ actortest/actortest.h
@@ -0,0 +1,19 @@
+//
+//  actortest.h
+//  actortest
+//
+//  Created by Julian Krumow on 09.11.15.
+//  Copyright © 2015 Julian Krumow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for actortest.
+FOUNDATION_EXPORT double actortestVersionNumber;
+
+//! Project version string for actortest.
+FOUNDATION_EXPORT const unsigned char actortestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <actortest/PublicHeader.h>
+
+
